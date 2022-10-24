7a73a2a23e8bb71c74d86e5e5aa0811a2ada704b
Cameron Edwards <91485465+CountryCam@users.noreply.github.com>
2022-10-24 12:52:47 +0100

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,89 @@
-basic.show_string("Hello")
-basic.show_string("Welcome Young Wizard")
+def on_received_number(receivedNumber):
+    global Bluetooth
+    Bluetooth = False
+    basic.clear_screen()
+    basic.show_string("" + str((receivedNumber)))
+    music.play_melody("C5 B A G G F E D ", 120)
+radio.on_received_number(on_received_number)
+
+def on_button_pressed_a():
+    global Red_Wizard, Play_Game, Blue_Wizard, Bluetooth
+    Red_Wizard = 1
+    radio.set_group(1)
+    radio.send_string("LOL")
+    radio.send_number(1)
+    Play_Game = 0
+    Blue_Wizard = 0
+    Bluetooth = True
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_ab():
+    global Play_Game, Blue_Wizard, Red_Wizard, Bluetooth
+    Play_Game = 1
+    Blue_Wizard = 0
+    Red_Wizard = 0
+    Bluetooth = True
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_received_string(receivedString):
+    global Bluetooth
+    Bluetooth = False
+    basic.clear_screen()
+    basic.show_string(receivedString)
+radio.on_received_string(on_received_string)
+
+def on_button_pressed_b():
+    global Blue_Wizard, Red_Wizard, Play_Game, Bluetooth
+    Blue_Wizard = 1
+    radio.set_group(2)
+    radio.send_number(2)
+    Red_Wizard = 0
+    Play_Game = 0
+    Bluetooth = True
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def Radio():
+    while Red_Wizard:
+        radio.set_group(1)
+    while Blue_Wizard:
+        radio.set_group(2)
+Blue_Wizard = 0
+Play_Game = 0
+Red_Wizard = 0
+Bluetooth = False
+radio.set_group(0)
+basic.show_string("HI")
+basic.clear_screen()
+Bluetooth = True
 
 def on_forever():
-    pass
+    if Red_Wizard:
+        while Red_Wizard:
+            if Bluetooth:
+                basic.show_leds("""
+                    . # # . .
+                                        . # . # .
+                                        . # # . .
+                                        . # . # .
+                                        . # . . #
+                """)
+            break
+    elif Play_Game:
+        while Play_Game:
+            if Bluetooth:
+                basic.show_icon(IconNames.PITCHFORK)
+            break
+    elif Blue_Wizard:
+        while Blue_Wizard:
+            if Bluetooth:
+                basic.show_leds("""
+                    . # # . .
+                                        . # . # .
+                                        . # # . .
+                                        . # . # .
+                                        . # # . .
+                """)
+            break
+    else:
+        pass
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
-radio.onReceivedNumber(function (receivedNumber) {
+radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
+    
     Bluetooth = false
     basic.clearScreen()
-    basic.showString("" + (receivedNumber))
+    basic.showString("" + ("" + receivedNumber))
     music.playMelody("C5 B A G G F E D ", 120)
 })
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
     Red_Wizard = 1
     radio.setGroup(1)
     radio.sendString("LOL")
@@ -13,18 +15,21 @@ input.onButtonPressed(Button.A, function () {
     Blue_Wizard = 0
     Bluetooth = true
 })
-input.onButtonPressed(Button.AB, function () {
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    
     Play_Game = 1
     Blue_Wizard = 0
     Red_Wizard = 0
     Bluetooth = true
 })
-radio.onReceivedString(function (receivedString) {
+radio.onReceivedString(function on_received_string(receivedString: string) {
+    
     Bluetooth = false
     basic.clearScreen()
     basic.showString(receivedString)
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
     Blue_Wizard = 1
     radio.setGroup(2)
     radio.sendNumber(2)
@@ -32,7 +37,7 @@ input.onButtonPressed(Button.B, function () {
     Play_Game = 0
     Bluetooth = true
 })
-function Radio () {
+function Radio() {
     while (Red_Wizard) {
         radio.setGroup(1)
     }
@@ -40,6 +45,7 @@ function Radio () {
         radio.setGroup(2)
     }
 }
+
 let Blue_Wizard = 0
 let Play_Game = 0
 let Red_Wizard = 0
@@ -48,41 +54,45 @@ radio.setGroup(0)
 basic.showString("HI")
 basic.clearScreen()
 Bluetooth = true
-basic.forever(function () {
+basic.forever(function on_forever() {
     if (Red_Wizard) {
         while (Red_Wizard) {
             if (Bluetooth) {
                 basic.showLeds(`
                     . # # . .
-                    . # . # .
-                    . # # . .
-                    . # . # .
-                    . # . . #
-                    `)
+                                        . # . # .
+                                        . # # . .
+                                        . # . # .
+                                        . # . . #
+                `)
             }
-            break;
+            
+            break
         }
     } else if (Play_Game) {
         while (Play_Game) {
             if (Bluetooth) {
                 basic.showIcon(IconNames.Pitchfork)
             }
-            break;
+            
+            break
         }
     } else if (Blue_Wizard) {
         while (Blue_Wizard) {
             if (Bluetooth) {
                 basic.showLeds(`
                     . # # . .
-                    . # . # .
-                    . # # . .
-                    . # . # .
-                    . # # . .
-                    `)
+                                        . # . # .
+                                        . # # . .
+                                        . # . # .
+                                        . # # . .
+                `)
             }
-            break;
+            
+            break
         }
     } else {
-    	
+        
     }
+    
 })
