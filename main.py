06602c0546222e983694a4c6f98de1d34ef118ac
Cameron Edwards <91485465+CountryCam@users.noreply.github.com>
2022-10-24 12:27:54 +0100

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,6 @@
+basic.show_string("Hello")
+basic.show_string("Welcome Young Wizard")
+
+def on_forever():
+    pass
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,88 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    Bluetooth = false
+    basic.clearScreen()
+    basic.showString("" + (receivedNumber))
+    music.playMelody("- - - - - - - - ", 120)
+})
+input.onButtonPressed(Button.A, function () {
+    Red_Wizard = 1
+    radio.setGroup(1)
+    radio.sendString("LOL")
+    radio.sendNumber(1)
+    Play_Game = 0
+    Blue_Wizard = 0
+    Bluetooth = true
+})
+input.onButtonPressed(Button.AB, function () {
+    Play_Game = 1
+    Blue_Wizard = 0
+    Red_Wizard = 0
+    Bluetooth = true
+})
+radio.onReceivedString(function (receivedString) {
+    Bluetooth = false
+    basic.clearScreen()
+    basic.showString(receivedString)
+})
+input.onButtonPressed(Button.B, function () {
+    Blue_Wizard = 1
+    radio.setGroup(2)
+    radio.sendNumber(2)
+    Red_Wizard = 0
+    Play_Game = 0
+    Bluetooth = true
+})
+function Radio () {
+    while (Red_Wizard) {
+        radio.setGroup(1)
+    }
+    while (Blue_Wizard) {
+        radio.setGroup(2)
+    }
+}
+let Blue_Wizard = 0
+let Play_Game = 0
+let Red_Wizard = 0
+let Bluetooth = false
+radio.setGroup(0)
+basic.showString("HI")
+basic.clearScreen()
+Bluetooth = true
+basic.forever(function () {
+    if (Red_Wizard) {
+        while (Red_Wizard) {
+            if (Bluetooth) {
+                basic.showLeds(`
+                    . # # . .
+                    . # . # .
+                    . # # . .
+                    . # . # .
+                    . # . . #
+                    `)
+            }
+            break;
+        }
+    } else if (Play_Game) {
+        while (Play_Game) {
+            if (Bluetooth) {
+                basic.showIcon(IconNames.Pitchfork)
+            }
+            break;
+        }
+    } else if (Blue_Wizard) {
+        while (Blue_Wizard) {
+            if (Bluetooth) {
+                basic.showLeds(`
+                    . # # . .
+                    . # . # .
+                    . # # . .
+                    . # . # .
+                    . # # . .
+                    `)
+            }
+            break;
+        }
+    } else {
+    	
+    }
+})
